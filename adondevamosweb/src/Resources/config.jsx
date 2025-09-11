3ce54ec6ca1c09e65bb4dfc672c3e5c76b4612ec
MoysAkuma <moises141294@hotmail.com>
2025-09-11 07:35:49 -0700

    add basehost env path to deploy

--- adondevamosweb/src/Resources/config.jsx
+++ adondevamosweb/src/Resources/config.jsx
@@ -1,7 +1,7 @@
 const config = {
   development: {
     api: {
-      baseUrl: 'http://localhost:3001',
+      baseUrl: process.env.APIBASEHOST || 'http://localhost:3001',
       site:{
         View : '/View'
       },
