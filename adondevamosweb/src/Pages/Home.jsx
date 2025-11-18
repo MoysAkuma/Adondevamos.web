b55be6dd8345cfb2bece9bc50d773ee258449072
MoysAkuma <moises141294@hotmail.com>
2025-11-17 22:17:57 -0700

    new version of api integration

--- adondevamosweb/src/Pages/Home.jsx
+++ adondevamosweb/src/Pages/Home.jsx
@@ -4,11 +4,18 @@ import
     {
         useMediaQuery,
         useTheme,
-        Container,
-        Typography
+        ButtonGroup,
+        Stack,
+        Avatar,
+        Typography,
+        Paper,
+        Button
     } from '@mui/material';
-import  NewTrips  from "../Component/Trips/NewTrips";
+import { FlightTakeoff, AccountCircle, AddLocation, Favorite, Flight, WhereToVote } from '@mui/icons-material';
 import CenteredTemplate from "../Component/Commons/CenteredTemplate";
+import { Box } from "@mui/system";
+import { styled } from '@mui/material/styles';
+
 
 function Home() {
     const theme = useTheme();
@@ -24,13 +31,61 @@ function Home() {
             <Typography variant="h5"  gutterBottom align="center">
               What is Adondevamos?
             </Typography>
-
-            <Typography variant="body1" gutterBottom align="center">
-              Is a website to create a list of places yo want to go on a trip and share with your friends.
+            <Typography variant="h5"  gutterBottom align="center">
+              How to use it?
             </Typography>
+             <Box sx={{ flexGrow: 3, overflow: 'hidden', px: 3 }}>
+                <Stack spacing={2} direction="row" sx={{ align: 'left', mb: 4 }}>
 
-            <NewTrips/>
+                  <Avatar>
+                    <FlightTakeoff />
+                  </Avatar>
+                  <Typography noWrap variant="p" > Add your trip</Typography>
+                </Stack>
 
+                <Stack spacing={2} direction="row" sx={{ alignContent:'right', mb: 4 }}>
+                  <Avatar>
+                    <AccountCircle />
+                  </Avatar>
+                  <Typography noWrap>Add your friends</Typography>
+                </Stack>
+
+                <Stack spacing={2} direction="row" sx={{ align: 'left', mb: 4 }}>
+                  <Avatar>
+                    <AddLocation />
+                  </Avatar>
+                  <Typography noWrap>Add places that you and your friend want to go</Typography>
+                </Stack>
+                
+                <Stack spacing={2} direction="row" sx={{ align: 'left', mb: 4 }}>
+                  <Avatar>
+                    <Favorite />
+                  </Avatar>
+                  <Typography noWrap>Vote and decide adondevamos</Typography>
+                </Stack>
+            </Box>
+            <Typography variant="h5"  gutterBottom align="center">
+              Where you want to go today?
+            </Typography>
+            <ButtonGroup 
+                        variant="contained" 
+                        color="primary" 
+                        fullWidth sx={{ mt: 2, mb: 4 }}> 
+              <Button 
+                variant="contained" 
+                startIcon={ <Flight/> }
+                href="/Trips" >
+                  Explore Trips 
+            </Button>
+
+            <Button 
+                variant="outlined" 
+                startIcon={ <WhereToVote/> }
+                href="/Places" >
+                  Explore Places 
+            </Button>
+
+            </ButtonGroup>
           </CenteredTemplate>
     );
   };
--- adondevamosweb/src/Resources/config.jsx
+++ adondevamosweb/src/Resources/config.jsx
@@ -1,7 +1,7 @@
 const config = {
   development: {
     api: {
-      baseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001',
+      baseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/v1',
       site:{
         View : '/View'
       },
